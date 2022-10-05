4a143ac20085a3b05079a8b0fd3fbc3ef5ff01d6
CathieLC <catherine-le-calve_student2021@wilder.school>
2022-10-05 19:34:25 +0200

    add py mapreduce pokemon

---
+++ Quêtes/HDFS/Pokemon_map_reduce.py
@@ -0,0 +1,53 @@
+from mrjob.job import MRJob
+
+from mrjob.step import MRStep
+
+
+
+class Types_count(MRJob):
+
+    def steps(self):
+
+        return [
+
+            MRStep(mapper=self.mapper_get_types,
+
+            reducer=self.reducer_types),
+
+            MRStep(reducer=self.reducer_max_count_types)
+
+        ]
+
+
+    def mapper_get_types(self, _, line):
+
+        (pokemonId,pokemonName,pokemonType1,pokemonType2,HP,Attack,Defense,SpAttack,SpDef,Speed,Generation,Lengendary) = line.split(',')
+
+        if pokemonId != "#":
+
+                yield (pokemonType1, (1, Defense))
+
+                
+
+
+    def reducer_types(self, key, values):
+
+        totalNumber,totalDefense=0,0
+
+        for value in values:
+
+            totalNumber += value[0]
+
+            totalDefense += int(value[1])
+
+        yield None, (totalDefense/totalNumber, key)
+
+
+    def reducer_max_count_types(self, _, values):
+
+        yield max(values)
+
+
+if __name__ == '__main__':
+
+    Types_count.run()
