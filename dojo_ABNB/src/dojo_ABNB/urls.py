145c88008247c564d0b874d6ea1f3ed18d28c0b1
CathieLC <catherine-le-calve_student2021@wilder.school>
2023-03-23 10:48:32 +0100

    add app listing

--- dojo_ABNB/src/dojo_ABNB/urls.py
+++ dojo_ABNB/src/dojo_ABNB/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from accounts.views import home
+
 urlpatterns = [
+    path("", home, name="home"),
     path("admin/", admin.site.urls),
+    # path("signup/", signup, name="signup"),
+    # path("login/", login_user, name="login"),
+    # path("logout/", logout_user, name="logout"),
 ]
