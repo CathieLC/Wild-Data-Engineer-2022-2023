e23126ba8c2d9dca3ee0eef904b70a7d13a2c42f
CathieLC <74502206+CathieLC@users.noreply.github.com>
2023-04-03 11:45:16 +0200

    add mission and cart

--- Projet Titre CDA/src/inventaire/views.py
+++ Projet Titre CDA/src/inventaire/views.py
@@ -9,22 +9,16 @@ def listePieces(request):
 
 
 def inventaireParLieu(request):
-    pass
-#     inventaireComplet = Inventaire_items.objects.all()
-#     typePiece = request.GET.get('place')  # Récupérer les paramètres de recherche
-#
-#     if typePiece:
-#         inventaireComplet = inventaireComplet.filter(place=typePiece)  # Filtrer les objets
-#
-#     return render(request, "inventaire/items_per_place.html", {"typePiece": inventaireComplet})
+    # pass
+    inventaireComplet = Inventaire_items.objects.all()
+    typePiece = request.GET.get('place')  # Récupérer les paramètres de recherche
+
+    if typePiece:
+        inventaireComplet = inventaireComplet.filter(place=typePiece)  # Filtrer les objets
+
+    return render(request, "inventaire/items_per_place.html", {"typePiece": inventaireComplet})
 
 
-def inventairetest(request):
-
-    inventaire = Inventaire_items.objects.annotate(piece_lower=Lower('place')).get()
-    inventairecomp = inventaire.piece_lower.distinct()
-
-    return render(request, "inventaire/test.html", {"inventairecomp": inventairecomp})
 
 
 
@@ -47,22 +41,22 @@ def inventairetest(request):
 - Utilisateur n'a pas encore de mission (il part de zéro)
 - A déjà des articles dans son panier, l'article y est déjà, il faut changer la quantité (incrémenter de 1)"""
 
-# def add_to_mission(request, items):
-#     # on récupère simplement l'utilisateur
-#     user = request.user
-#     inventaire = get_object_or_404(Inventaire_items, items=items)
-#     contenu, _ = Contenu.objects.get_or_create(user=user) #si le contenu n'existe pas il sera créé
-#
-#     # order ce sera l'article à récupérer si il existe déjà dans le contenu
-#     # created permettra à l'article d'être créé dans le contenu si ce n'est pas le cas
-#     item, created = Mission.objects.get_or_create(user=user,
-#                                                   dejaCommande=False,
-#                                                   article=article)
-#     if created:
-#         contenu.orders.add(order)
-#         contenu.save()
-#     else:
-#         contenu.quantity +=1
-#         contenu.save()
-#
-#     return redirect(reverse("pieceDetails", kwargs={"slug": slug}))
+def add_to_mission(request, items):
+    # on récupère simplement l'utilisateur
+    user = request.user
+    article = get_object_or_404(Inventaire_items, items=items)
+    contenu, _ = Contenu.objects.get_or_create(user=user) #si le contenu n'existe pas il sera créé
+
+    # order ce sera l'article à récupérer si il existe déjà dans le contenu
+    # created permettra à l'article d'être créé dans le contenu si ce n'est pas le cas
+    mission, created = Mission.objects.get_or_create(user=user,
+                                                     article=article)
+    if created:
+        contenu.missions.add(item)
+        contenu.save()
+    else:
+        contenu.quantite +=1
+        contenu.save()
+        contenu.save()
+
+    return redirect(reverse("inventaire-par-lieu"))
